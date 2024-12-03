ecd032c147ad9e265cda8b952f31991e055c8064
Divyesh Gangera <142652329+divyeshgangara2211@users.noreply.github.com>
2024-12-03 11:57:51 +0530

    Update assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -1,3 +1,8 @@
+// 2. Assignment Operators
+// Challenge: Write a program that allows a user to input the number of tea bags they have. Assign additional bags to them based on certain conditions (e.g., if they have fewer than 10 bags, give them 5 extra). Update the original number using assignment operators.
+
+// Operators: +=, -=, *=, /=, %=
+
 #include<iostream>
 
 using namespace std;
