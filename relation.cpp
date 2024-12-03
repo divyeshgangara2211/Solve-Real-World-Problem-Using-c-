4737a0397bea3b01d0467b1c54330e74dc81191f
Divyesh Gangera <142652329+divyeshgangara2211@users.noreply.github.com>
2024-12-03 11:59:34 +0530

    Update relation.cpp

--- relation.cpp
+++ relation.cpp
@@ -1,3 +1,8 @@
+// 3. Relational Operators
+
+// Challenge: A tea shop offers a loyalty program. Customers who buy more than 20 cups of tea get a special "Gold" badge, and those who buy 10 to 20 cups get a "Silver" badge. Write a program to display the badge they will receive based on the number of cups they buy.
+// Operators: >, <, >=, <=
+
 #include<iostream>
 
 using namespace std;
